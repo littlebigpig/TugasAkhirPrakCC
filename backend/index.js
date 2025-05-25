87829a366684535ee2d8e894a77a8008ceedb0c6
RaihanR <reyhanridho12@gmail.com>
2025-05-25 21:45:56 +0700

    ya

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('API Warnet siap digunakan!');
 });
 
-export default app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
